faef4599e4cf51cce287ea64d46ebfdde1909875
Malissa Martinez de Murga <37825977+malissam@users.noreply.github.com>
2021-02-12 16:00:45 -0800

    camera follows kyle

--- HW2/Assets/MoveKyle.cs
+++ HW2/Assets/MoveKyle.cs
@@ -1,22 +1,28 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿
 using UnityEngine;
 
 public class MoveKyle : MonoBehaviour
 {
-    public Rigidbody kyle;
+    public Rigidbody rb;
     public float forwardForce = 2000f;
-    public float sidewaysForce = 500f;
-    void FixedUpdaate()
+    public float sidewaysForce = 50f;
+
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    void FixedUpdate()
     {
-        if( Input.GetKey("d") )
+
+
+        if (Input.GetKey("d"))
         {
-            kyle.AddForce(sidewaysForce,0,0);
+            Debug.Log("d");
+            rb.AddForce(sidewaysForce,0,0);
         }
-         if( Input.GetKey("a") )
+        if (Input.GetKey("a"))
         {
-            kyle.AddForce(sidewaysForce,0,0);
+             Debug.Log("a");
+            rb.AddForce(-sidewaysForce,0,0);
         }
     }
-
 }
