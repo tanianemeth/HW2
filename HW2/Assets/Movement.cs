b75194c458c334e071b0404e9efe5cf51d328dce
Malissa Martinez de Murga <37825977+malissam@users.noreply.github.com>
2021-02-15 22:59:27 -0800

    animation with character movement

---
+++ HW2/Assets/Movement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private CharacterController characterController;
+    private Animator animator;
+    public float speed = 1f;
+    void Awake()
+    {
+        characterController = GetComponentInChildren<CharacterController>();
+        animator = GetComponentInChildren<Animator>();
+    }
+
+
+    void Update()
+    {
+        var forward = Input.GetAxis("Horizontal") * speed;
+        var sideways = Input.GetAxis("Vertical") * speed;
+
+        characterController.SimpleMove(new Vector3(forward, 0,sideways));
+        
+        if(!characterController.isGrounded)
+        {
+            characterController.SimpleMove(new Vector3(0,-8,0));
+        }
+
+        animator.SetFloat("speed",characterController.velocity.magnitude);
+    }
+}
