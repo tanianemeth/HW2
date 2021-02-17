47115a535a04a1d10e15ad86e65817c6195cee66
Malissa Martinez de Murga <37825977+malissam@users.noreply.github.com>
2021-02-17 02:24:13 -0800

    movement works
    walk, idle punch

--- HW2/Assets/Movement.cs
+++ HW2/Assets/Movement.cs
@@ -11,6 +11,7 @@ public class Movement : MonoBehaviour
    
     void Awake()
     {
+        //better way of getting component, this way object can spawm with all the right components. 
         characterController = GetComponentInChildren<CharacterController>();
         animator = GetComponentInChildren<Animator>();
     }
@@ -18,26 +19,35 @@ public class Movement : MonoBehaviour
 
     void Update()
     {
-        var forward = Input.GetAxis("Horizontal") * speed;
+        // w,s or updown. made for a controller joystick, think forward on the joystick
+        var forward = Input.GetAxis("Horizontal") * speed; 
+        //a , d or right left. this is the x axis on the joystick 
         var sideways = Input.GetAxis("Vertical") * speed;
 
-        characterController.SimpleMove(new Vector3(forward, 0,sideways));
+        //simple move
+        characterController.SimpleMove(new Vector3(forward,3,sideways));
         
+    // our gravity for our player
+    // https://docs.unity3d.com/ScriptReference/CharacterController.Move.html
         if(!characterController.isGrounded)
         {
-            characterController.SimpleMove(new Vector3(0,-8,0));
+            characterController.SimpleMove(new Vector3(0,-9,0));
         }
     
+    // set trigger activated the animation
         if (Input.GetKey("q"))
         {
             Debug.Log("q");
             animator.SetTrigger("shoot");
             
         }
+        //if character moves, reset the punch trigger for the animation
         if(forward > 0.01 || sideways > 0.01 )
         {
              animator.ResetTrigger("shoot");
         }
+
+        //how to set a wait time, like after 2 seconds reset trigger
   
         animator.SetFloat("speed",characterController.velocity.magnitude);
     }
