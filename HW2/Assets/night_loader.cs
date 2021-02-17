f8c618655295d1ef0b556ad6b1d3e5c87752b68c
tanianemeth <tnemeth@pipeline.sbcc.edu>
2021-02-16 21:33:31 -0800

    day to night
    added day to night and night to day with clicks in scene 2

---
+++ HW2/Assets/night_loader.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class night_loader : MonoBehaviour
+{
+  public Animator transition;
+  public float transitionTime = 1f;
+  //triggering level change with button
+
+
+
+    void Update()
+    {
+      if(Input.GetMouseButtonDown(0)){
+
+        load_night();
+      }
+
+    }
+    public void load_night(){
+        transition.SetTrigger("night_time");
+
+    }
+
+
+}
