831811ba5c90ff0e1828841d1018d2cc6290fdb3
tanianemeth <tnemeth@pipeline.sbcc.edu>
2021-02-17 21:38:26 -0800

    particles and textures
    add particle effect on cube when space bar is pressed and texture change to sphere when button is pressed

---
+++ HW2/Assets/ChangeMaterial.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeMaterial : MonoBehaviour
+{
+  public GameObject sphere;
+
+  public Material[] textures;
+
+  private int randomTextureIndex;
+    // Start is called before the first frame update
+    void Start()
+    {
+      gameObject.GetComponent<Button>().onClick.AddListener(ChangeSphereTexture);
+
+    }
+
+    // Update is called once per frame
+    void ChangeSphereTexture()
+    {
+      Renderer sphereRenderer = sphere.GetComponent<Renderer>();
+      randomTextureIndex = Random.Range(0, textures.Length);
+      sphereRenderer.material = textures[randomTextureIndex];
+
+    }
+}
---
+++ HW2/Assets/Cube.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+  [SerializeField] ParticleSystem collectParticle = null;
+
+  public void Update(){
+    if (Input.GetKeyDown(KeyCode.Space)){
+      Collect();
+    }
+  }
+
+  public void Collect(){
+    //play particle graphics
+    collectParticle.Play();
+  }
+}
